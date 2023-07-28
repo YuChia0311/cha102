bbf22e69598aede96a682d3cea49d9fda55c0bc8
YuChia0311 <jo1397200@gmail.com>
2023-07-28 15:09:51 +0800

    xxxxxxxxxxxx

--- src/main/java/test/controller/TestController.java
+++ src/main/java/test/controller/TestController.java
@@ -16,6 +16,7 @@ public class TestController extends HttpServlet{
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		var xxx = req.getParameter("xxx");
 		req.setAttribute("nickname", xxx);
+		
 		req.getRequestDispatcher("/index.jsp").forward(req, resp);
 	}
 	
